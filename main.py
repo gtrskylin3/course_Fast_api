174a7eda1b8d56914d021b75fa12794a70a49331
Mctraxer228 <cernovdaniil@seniorpro.com>
2025-08-08 00:48:09 +0700

    pydantic

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from fastapi import FastAPI, status, Body
 from uvicorn import run
+from schemas import Message
 app = FastAPI()
 
-messages_db = {"0": "First post in FastAPI"}
+messages_db = []
 
 
 @app.get("/")
 async def get_all_messages() -> dict:
-    return messages_db
+    return {'Messages': messages_db}
 
 
 @app.get("/message/{message_id}")
-async def get_message(message_id: str) -> str:
+async def get_message(message_id: int):
      return messages_db[message_id]
 
 
 @app.post("/message", status_code=status.HTTP_201_CREATED)
-async def create_message(message: str = Body()) -> str:
-    current_index = len(messages_db)
-    messages_db[current_index] = message
+async def create_message(message: Message) -> str:
+    if len(messages_db) == 0:
+        message.id = 0
+    else:
+        message.id = max([i.dict()['id'] for i in messages_db]) + 1
+    messages_db.append(message)
     return f"Message created!"
 
 @app.put("/message/{message_id}")
