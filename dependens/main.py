95fc4f28e85c9339db37fac1bfcd31f89a9773ca
Mctraxer228 <cernovdaniil@seniorpro.com>
2025-08-08 17:45:14 +0700

    depends module

--- dependens/main.py
+++ dependens/main.py
@@ -8,7 +8,17 @@ class Post(BaseModel):
 
 db = []
 
-app = FastAPI()
+
+from fastapi import Request
+
+# app = FastAPI()
+
+log_user = []
+
+def log_client(request: Request):
+    log_user.append(request.headers)
+
+app = FastAPI(dependencies=[Depends(log_client)]) # global
 
 async def get_post_or_404(id: int):
     try:
@@ -28,6 +38,33 @@ async def create_message(post: Post) -> str:
     db.append(post)
     return f"Message Created"
 
+class Paginator:
+    def __init__(self, limit: int = 10, page: int = 1):
+        self.limit = limit
+        self.page = page
+    
+    
+    def __call__(self, limit: int):
+        if limit < self.limit:
+            return [{'limit': self.limit, 'page': self.page}]
+        else:
+            return [{'limit': limit, 'page': self.page}]
+
+my_paginator = Paginator()
+
+
+@app.get("/users")
+async def all_users(pagination: Paginator = Depends(my_paginator)):
+    return {"user": pagination}
+
+
+async def pagination_path_func(page: int):
+    if page < 0:
+        raise HTTPException(status_code=404, detail="Page not exist")
+    if page == 0:
+        raise HTTPException(status_code=500,
+                            detail="Invalid page value")
+
 
 async def pagintation_func(limit: int = Query(10, ge=0), 
                            page: int = 1):
@@ -35,7 +72,7 @@ async def pagintation_func(limit: int = Query(10, ge=0),
 
 
 
-@app.get("/messages")
+@app.get("/messages", dependencies=[Depends(pagination_path_func)])
 async def all_messages(pagination: dict = Depends(pagintation_func)):
     return {"messages": pagination}
 
@@ -43,6 +80,27 @@ async def all_messages(pagination: dict = Depends(pagintation_func)):
 @app.get("/comments")
 async def all_messages(pagination: dict = Depends(pagintation_func)):
     return {"messages": pagination}
+
+
+async def sub_dependency(request: Request) -> str:
+    return request.method
+
+async def main_dependency(sub_dependency_value: str = Depends(sub_dependency)) -> str:
+    return sub_dependency_value
+
+@app.get("/test/")
+def test_endpoint(test: str = Depends(main_dependency)):
+    return test
+
+@app.get("/log_user")
+async def print_log_user():
+    return {"user": log_user}
+
+
+
+
+
+
 
 if __name__ == "__main__":
     run("main:app", reload=True)
