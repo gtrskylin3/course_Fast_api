035c03a13939b4291b024a92461d529a095a4d06
daniil <wwcern0vdan11lzxc@example.com>
2025-08-11 21:17:53 +0700

    permission add supplier_permission func for check user is_supplier

--- shop_fastapi/app/main.py
+++ shop_fastapi/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routers.category import router as category
 from app.routers.products import router as products
 from app.routers.auth import router as auth
+from app.routers.permission import router as permission
 from app.models.category import Category
 from app.models.products import Product
 from sqlalchemy.schema import CreateTable
@@ -17,3 +18,4 @@ async def welcome() -> dict:
 app.include_router(category)
 app.include_router(products)
 app.include_router(auth)
+app.include_router(permission)
---
+++ shop_fastapi/app/routers/permission.py
@@ -0,0 +1,49 @@
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy import select, update
+from .auth import get_current_user
+from app.models import User
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.backend.db_depends import get_db
+
+router = APIRouter(prefix="/permission", tags=["permission"])
+session = Annotated[AsyncSession, Depends(get_db)]
+
+
+@router.patch("/")
+async def supplier_permission(
+    db: session,
+    get_user: Annotated[dict, Depends(get_current_user)],
+    user_id: int,
+):
+    if get_user.get("is_admin"):
+        user = await db.scalar(select(User).where(User.id == user_id))
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+            )
+        if user.is_supplier:
+            await db.execute(update(User).where(User.id == user_id)).values(
+                is_supplier=False,
+                is_customer=True,
+            )
+            await db.commit()
+            return {
+                'status_code': status.HTTP_200_OK,
+                'detail': 'User is no longer supplier'
+            }
+        else:
+            await db.execute(update(User).where(User.id == user_id).values(
+                is_supplier=True,
+                is_customer=False,
+            ))
+            await db.commit()
+            return {
+                'status_code': status.HTTP_200_OK,
+                'detail': 'User is now supplier'
+            }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You don't have admin permission"
+        )
